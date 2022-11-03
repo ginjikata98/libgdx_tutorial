570af8ec666f08218a054e6813003a5eea10c576
Vũ Minh Dũng <dung.vu@finhay.com.vn>
2022-11-03 13:48:34 +0700

    add actor and turtle class

---
+++ core/src/com/mygdx/game/starfish/ActorBeta.java
@@ -0,0 +1,52 @@
+package com.mygdx.game.starfish;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class ActorBeta extends Actor {
+  private TextureRegion textureRegion;
+  private Rectangle rectangle;
+
+  public ActorBeta() {
+    super();
+    textureRegion = new TextureRegion();
+    rectangle = new Rectangle();
+  }
+
+  public void setTexture(Texture t) {
+    textureRegion.setRegion(t);
+    setSize(t.getWidth(), t.getHeight());
+    rectangle.setSize(t.getWidth(), t.getHeight());
+  }
+
+  public Rectangle getRectangle() {
+    rectangle.setPosition(getX(), getY());
+    return rectangle;
+  }
+
+  public boolean overlaps(ActorBeta other) {
+    return this.getRectangle().overlaps(other.getRectangle());
+  }
+
+  public void act(float dt) {
+    super.act(dt);
+  }
+
+  public void draw(Batch batch, float parentAlpha) {
+    super.draw(batch, parentAlpha);
+
+    var color = getColor();
+    batch.setColor(color.r, color.g, color.b, color.a);
+
+    if (isVisible()) {
+      batch.draw(textureRegion, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(),
+          getScaleY(), getRotation());
+    }
+
+  }
+
+
+}
